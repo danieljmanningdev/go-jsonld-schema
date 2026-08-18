c7d6cc76c97132f06cf10f3c71c88ac0e0b14c1d
SympticDevelopment <developer@symptic.co.uk>
2026-08-18 20:08:03 +0100

    feat: add types

---
+++ schema/types.go
@@ -0,0 +1,18 @@
+package schema
+
+const (
+	TypeThing = "Thing"
+
+	TypePerson       = "Person"
+	TypeOrganization = "Organization"
+	TypeArticle      = "Article"
+
+	TypeWebSite = "WebSite"
+	TypeWebPage = "WebPage"
+
+	TypeBreadcrumbList = "BreadcrumbList"
+	TypeListItem       = "ListItem"
+
+	TypeImageObject  = "ImageObject"
+	TypeSearchAction = "SearchAction"
+)
