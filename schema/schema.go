efb7207ae27ff9f73f3b7338bc7052a4176c5cda
SympticDevelopment <developer@symptic.co.uk>
2026-08-18 15:59:09 +0100

    feat: add JSON-LD marshaling helpers

--- schema/schema.go
+++ schema/schema.go
@@ -1,1 +1,11 @@
 package schema
+
+import "encoding/json"
+
+func Marshal(v any) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+func MarshalIndent(v any) ([]byte, error) {
+	return json.MarshalIndent(v, "", "  ")
+}
