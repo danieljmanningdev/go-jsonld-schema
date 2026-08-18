87768e960d84610866b3ee6492832a1479f97d2c
SympticDevelopment <developer@symptic.co.uk>
2026-08-18 15:46:43 +0100

    feat: add article schema

--- schema/article.go
+++ schema/article.go
@@ -1,1 +1,40 @@
 package schema
+
+type Article struct {
+	Root
+
+	Headline      string     `json:"headline"`
+	Image         string     `json:"image,omitempty"`
+	Author        *Author    `json:"author,omitempty"`
+	Publisher     *Publisher `json:"publisher,omitempty"`
+	DatePublished string     `json:"datePublished,omitempty"`
+}
+
+type Author struct {
+	Typed
+
+	Name string `json:"name"`
+}
+
+type Publisher struct {
+	Typed
+
+	Name string       `json:"name"`
+	Logo *ImageObject `json:"logo,omitempty"`
+}
+
+type ImageObject struct {
+	Typed
+
+	URL string `json:"url"`
+}
+
+func NewArticle(headline string) Article {
+	return Article{
+		Root: Root{
+			Context: "https://schema.org",
+			Type:    "Article",
+		},
+		Headline: headline,
+	}
+}
