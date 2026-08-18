0e7abe504d438a941a56b5bf32b3b4167bb4e6a9
SympticDevelopment <developer@symptic.co.uk>
2026-08-18 15:27:11 +0100

    feat: add person schema

--- schema/person.go
+++ schema/person.go
@@ -1,1 +1,31 @@
 package schema
+
+type SocialProfile string
+
+type Person struct {
+	Root
+
+	Name           string          `json:"name"`
+	URL            string          `json:"url,omitempty"`
+	PictureURL     string          `json:"image,omitempty"`
+	SocialProfiles []SocialProfile `json:"sameAs,omitempty"`
+	JobTitle       string          `json:"jobTitle,omitempty"`
+	WorksFor       *Organisation   `json:"worksFor,omitempty"`
+}
+
+type Organisation struct {
+	Typed
+
+	Name string `json:"name"`
+	URL  string `json:"url,omitempty"`
+}
+
+func NewPerson(name string) Person {
+	return Person{
+		Root: Root{
+			Context: "https://schema.org/",
+			Type:    "Person",
+		},
+		Name: name,
+	}
+}
