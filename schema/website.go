e55d092853ab3abfb7044bbdfacb0355457f2fe9
SympticDevelopment <developer@symptic.co.uk>
2026-08-18 15:13:07 +0100

    feat: add WebSite schema

--- schema/website.go
+++ schema/website.go
@@ -1,1 +1,25 @@
 package schema
+
+type WebSite struct {
+	Context string `json:"@context"`
+	Type    string `json:"@type"`
+	Name    string `json:"name"`
+	URL     string `json:"url"`
+
+	PotentialAction *SearchAction `json:"potentialAction,omitempty"`
+}
+
+type SearchAction struct {
+	Type       string `json:"@type"`
+	Target     string `json:"target"`
+	QueryInput string `json:"query-input"`
+}
+
+func NewWebsite(name, url string) WebSite {
+	return WebSite{
+		Context: "https://schema.org",
+		Type:    "WebSite",
+		Name:    name,
+		URL:     url,
+	}
+}
