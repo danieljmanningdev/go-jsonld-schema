0fdbec3dbb5e50cfe6d958c55fc3db990ea792d1
SympticDevelopment <developer@symptic.co.uk>
2026-08-18 15:45:43 +0100

    feat: add breadcrumb schema

--- schema/breadcrumb.go
+++ schema/breadcrumb.go
@@ -1,1 +1,25 @@
 package schema
+
+type BreadcrumbList struct {
+	Root
+
+	ItemListElement []ListItem `json:"itemListElement"`
+}
+
+type ListItem struct {
+	Typed
+
+	Position int    `json:"position"`
+	Name     string `json:"name"`
+	Item     string `json:"item"`
+}
+
+func NewBreadcrumbList(items []ListItem) BreadcrumbList {
+	return BreadcrumbList{
+		Root: Root{
+			Context: "https://schema.org/",
+			Type:    "BreadcrumbList",
+		},
+		ItemListElement: items,
+	}
+}
