c55f558289209c0c77ebb5361a17a2c2c20b9ea7
SympticDevelopment <developer@symptic.co.uk>
2026-08-18 15:37:18 +0100

    feat: add organisation schema

--- schema/organisation.go
+++ schema/organisation.go
@@ -1,1 +1,20 @@
 package schema
+
+type Organisation struct {
+	Root
+
+	Name string `json:"name"`
+	URL  string `json:"url,omitempty"`
+	Logo string `json:"logo,omitempty"`
+}
+
+func NewOrganisation(name, url string) Organisation {
+	return Organisation{
+		Root: Root{
+			Context: "https://schema.org",
+			Type:    "Organization",
+		},
+		Name: name,
+		URL:  url,
+	}
+}
