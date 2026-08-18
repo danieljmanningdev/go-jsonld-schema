587249ac79e1447a2a6961c243c1ca6f54d5c385
SympticDevelopment <developer@symptic.co.uk>
2026-08-18 16:17:35 +0100

    refactor: add primitive ID

--- schema/base.go
+++ schema/base.go
@@ -1,10 +1,12 @@
 package schema
 
 type Typed struct {
+	ID   string `json:"@id,omitempty"`
 	Type string `json:"@type"`
 }
 
 type Root struct {
 	Context string `json:"@context,omitempty"`
+	ID      string `json:"@id,omitempty"`
 	Type    string `json:"@type"`
 }
