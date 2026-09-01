80f060d1612188ba6c1723a4a5aa4dba1ff962cd
Daniel J. Manning <daniel@danieljmanningdev.com>
2026-09-02 00:18:57 +0100

    feat: add service schema type constants

--- schema/types.go
+++ schema/types.go
@@ -3,9 +3,12 @@ package schema
 const (
 	TypeThing = "Thing"
 
-	TypePerson       = "Person"
-	TypeOrganization = "Organization"
-	TypeArticle      = "Article"
+	TypePerson              = "Person"
+	TypeOrganization        = "Organization"
+	TypeArticle             = "Article"
+	TypeService             = "Service"
+	TypeProfessionalService = "ProfessionalService"
+	TypeOffer               = "Offer"
 
 	TypeWebSite = "WebSite"
 	TypeWebPage = "WebPage"
